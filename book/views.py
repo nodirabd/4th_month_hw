d7c0656ac332711d7ffa45aa8bb286f9cc38ca83
nodiraabd <abdurakhmanovanodirabegim@gmail.com>
2026-04-28 16:43:13 +0600

    changed some citiations

--- book/views.py
+++ book/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def citation1(request):
-    return HttpResponse("Бла бла бал")
+    return HttpResponse("Ведь все взрослы были детьми, только мало кто из них об этом помнит.") #маленький принц
 def citation2(request):
-    return HttpResponse("Бла бла бал")
+    return HttpResponse("Все приходит во время для того кто умеет ждать.") # война и мир Лев Толстой
 def citation3(request):
-    return HttpResponse("Бла бла бал")
+    return HttpResponse("Муки и слезы - ведь это тоже жизнь.") # Анна Каренина Лев Толстой
